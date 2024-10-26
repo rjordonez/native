c9c684c46b236252838c0976ca1bf848f190973d
Rex Ordonez <rexordonez@Rexs-MacBook-Pro.local>
2024-10-26 08:04:22 -0700

    Configure for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/native/' // Replace 'repository' with your actual GitHub repository name
 });
